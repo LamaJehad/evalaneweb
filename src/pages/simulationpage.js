dda763b19c41ce6f85f7e18382df6b2795a084ce
LamaJehad <lamaalraie2004@gmail.com>
2026-06-10 13:48:49 +0300

    Update API URL

--- src/pages/simulationpage.js
+++ src/pages/simulationpage.js
@@ -943,7 +943,7 @@ export default function EvalaneSimPage() {
         const dominant = Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b);
 
         const local = computeAI(counts, sim.ambulance);
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+        const response = await fetch("https://evalane-api.onrender.com/predict", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
